ce99c0ecf44e7163a2616b0564558ab137586c96
Marc <g40419@gmail.com>
2020-07-01 00:11:25 +0800

    feat: add signup function & 資料驗證

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,14 +4,33 @@ const User = db.User
 
 const userController = {
   signUpPage: (req, res) => res.render('signup'),
-  signUp: (req, res) => {
-    const { name, email, password } = req.body
-    User.create({
-      name,
-      email,
-      password: bcrypt.hashSync(password, bcrypt.genSaltSync(10), null)
-    })
-    .then(user => res.redirect('/signin'))
+  signUp: async (req, res) => {
+    const { name, email, password, passwordCheck } = req.body
+    const findEmail = await User.findOne({ where: { email } })
+    try {
+      if (passwordCheck !== password) {
+        req.flash('error_msg', '兩次密碼不同！')
+        return res.redirect('/signup')
+      } else {
+        if (findEmail) {
+          req.flash('error_msg', '已經註冊過了！')
+          return res.redirect('/signup')
+        } else {
+          User.create({
+            name,
+            email,
+            password: bcrypt.hashSync(password, bcrypt.genSaltSync(10), null)
+          })
+            .then(user => {
+              req.flash('success_msg', '成功註冊！')
+              res.redirect('/signin')
+            })
+            .catch(err => console.log('create err', err))
+        }
+      }
+    } catch (err) {
+      console.log(err)
+    }
   }
 }
 
