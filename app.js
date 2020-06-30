2f89de692409d3e9303af9acfe797639a0ccab45
Marc <g40419@gmail.com>
2020-06-30 22:50:02 +0800

    feat: sequelize init

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express()
 const port = 3000
 
 const exphbs = require('express-handlebars')
+const db = require('./models')
 
 app.engine('hbs', exphbs({ defaultLayout:'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
