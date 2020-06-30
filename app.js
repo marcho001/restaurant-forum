8af9ddf450233bc3bf73d58cdb888e7d47501501
Marc <g40419@gmail.com>
2020-06-30 21:42:07 +0800

    feat: add controllers

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port = 3000
 
 const exphbs = require('express-handlebars')
 
-app.engine('handlebars', exphbs({ defaultLayout:'main' }))
+app.engine('handlebars', exphbs({ defaultLayout:'main', extname: 'hbs' }))
 app.set('view engine', 'handlebars')
 
 app.listen(port, () => {
