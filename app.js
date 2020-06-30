9cee9f070722e461a0159bc39f0ac1f4e21b8075
Marc <g40419@gmail.com>
2020-06-30 21:32:57 +0800

    initialize project

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const exphbs = require('express-handlebars')
+
+app.engine('handlebars', exphbs({ defaultLayout:'main' }))
+app.set('view engine', 'handlebars')
+
+app.listen(port, () => {
+  console.log('now is running')
+})
