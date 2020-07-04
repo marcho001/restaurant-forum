537f864559b3a8f35ef672bb5b3a1dafecd7ae98
Marc <g40419@gmail.com>
2020-07-04 13:04:34 +0800

    feat: add admin restaurant delete feature

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -67,6 +67,15 @@ let adminController = {
           res.redirect('/admin/restaurants')
         })
       })
+  },
+  deleteRestaurant: (req, res) => {
+    return Restaurant.findByPk(req.params.id)
+      .then(restaurant => {
+        restaurant.destroy()
+        res.redirect('/admin/restaurants')
+          // .then(restaurant => res.redirect('/admin/restaurants'))
+      })
+      .catch(err => console.log(err))
   }
 }
 
