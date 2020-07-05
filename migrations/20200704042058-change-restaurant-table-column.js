fcce762be75dddcd75f4095cf8aae20ac7e96d77
Marc <g40419@gmail.com>
2020-07-05 11:13:35 +0800

    push heroku

--- migrations/20200704042058-change-restaurant-table-column.js
+++ migrations/20200704042058-change-restaurant-table-column.js
@@ -1,23 +1,23 @@
-'use strict';
+// 'use strict';
 
-module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    await queryInterface.renameColumn('Restaurants', 'opening_hour', 'opening_hours')
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
-  },
+// module.exports = {
+//   up: async (queryInterface, Sequelize) => {
+//     await queryInterface.renameColumn('Restaurants', 'opening_hour', 'opening_hours')
+//     /**
+//      * Add altering commands here.
+//      *
+//      * Example:
+//      * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+//      */
+//   },
 
-  down: async (queryInterface, Sequelize) => {
-    await queryInterface.renameColumn('Restaurants', 'opening_hours', 'opening_hour')
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  }
-};
+//   down: async (queryInterface, Sequelize) => {
+//     await queryInterface.renameColumn('Restaurants', 'opening_hours', 'opening_hour')
+//     /**
+//      * Add reverting commands here.
+//      *
+//      * Example:
+//      * await queryInterface.dropTable('users');
+//      */
+//   }
+// };
