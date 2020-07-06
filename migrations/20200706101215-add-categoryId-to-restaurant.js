195dadf5799fc4a062e6892c11e522e4b9abadd3
Marc <g40419@gmail.com>
2020-07-06 18:33:15 +0800

    create category model & add restaurant.CategoryId and association

---
+++ migrations/20200706101215-add-categoryId-to-restaurant.js
@@ -0,0 +1,30 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.addColumn('Restaurants', 'CategoryId', {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      reference: {
+        model: 'Categories',
+        key: 'id'
+      }
+    })
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.removeColumn('Restaurants', 'CategoryId')
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
