4601f454395186edc1f1524531e65f2d08c5f816
Marc <g40419@gmail.com>
2020-07-06 20:35:05 +0800

    add category seed data

--- seeders/20200704091351-my-seed-file.js
+++ seeders/20200704091351-my-seed-file.js
@@ -26,6 +26,15 @@ module.exports = {
         updatedAt: new Date()
       }], {})
 
+    queryInterface.bulkInsert('Categories', ['中式料理', '日本料理', '義大利料理', '墨西哥料理', '素食料理', '美式料理', '複合式料理'].map((item, index) => ({
+      id: index + 1,
+      name: item,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    })
+    ), {}
+    )
+
       return queryInterface.bulkInsert('Restaurants', Array.from({ length: 50 }).map(d => ({
         name: faker.name.findName(),
         tel: faker.phone.phoneNumber(),
@@ -34,7 +43,8 @@ module.exports = {
         image: `https://loremflickr.com/320/240/restaurant,food/?random=${Math.random() * 100}`,
         description: faker.lorem.text(),
         createdAt: new Date(),
-        updatedAt: new Date()
+        updatedAt: new Date(),
+        CategoryId: Math.floor(Math.random() * 5) + 1
       })
       ), {})
     /**
@@ -50,6 +60,7 @@ module.exports = {
 
   down: async (queryInterface, Sequelize) => {
     queryInterface.bulkDelete('Users', null, {})
+    queryInterface.bulkDelete('Categories', null, {})
     return queryInterface.bulkDelete('Restaurants', null, {})
     /**
      * Add commands to revert seed here.
